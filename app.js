8b175bb7e720fdb15505eb86bc0d9db0a76f972b
Rafael Pili <rafael.pili@movile.com>
2022-07-13 19:39:41 -0300

    feat: add jwt

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const uri = "mongodb+srv://rafapili1:KJVHVnoRapNXV48z@cluster0.a4dwr.mongodb.net
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require('uuid');
+var jwt = require('jsonwebtoken');
+var fs = require('fs')
 
 var express = require('express');
+
+const RSA_PRIVATE_KEY = fs.readFileSync('./demos/mykey.pem');
 
 let dbConnection;
 
@@ -25,6 +29,8 @@ async function app() {
   //Here we are configuring express to use body-parser as middle-ware.
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  app.route('/api/login')
+  .post(loginRoute);
 
   app.post('/patient', async function (req, res) {
     dbConnection.collection("patient")
@@ -64,6 +70,30 @@ async function app() {
   app.listen((process.env.PORT || 3000), function () {
     console.log('server listening on port 3000!');
   });
+}
+
+function loginRoute(req, res) {
+
+  const email = req.body.email,
+        password = req.body.password;
+
+  if (validateEmailAndPassword()) {
+    //  const userId = findUserIdForEmail(email);
+
+      const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+              algorithm: 'RS256',
+              expiresIn: 120,
+              subject: email
+          })
+
+          res.status(200).json({
+            idToken: jwtBearerToken, 
+            expiresIn: 1000000
+          });                            
+  } else {
+      // send status 401 Unauthorized
+      res.sendStatus(401); 
+  }
 }
 
 app();
